4d59c6784dc76f293c324a801fdeefe1d9eb304a
David Gutheinz <djgutheinz@outlook.com>
2020-04-20 15:47:36 -0500

    Update wLightBoxMono.groovy

--- bleBoxDevices/Drivers/wLightBoxMono.groovy
+++ bleBoxDevices/Drivers/wLightBoxMono.groovy
@@ -21,7 +21,7 @@ metadata {
 	definition (name: "bleBox wLightBox Mono",
 				namespace: "davegut",
 				author: "Dave Gutheinz",
-				importUrl: "https://raw.githubusercontent.com/DaveGut/bleBox-Hubitat/master/Drivers/wLightBoxMono.groovy"
+				importUrl: "https://raw.githubusercontent.com/DaveGut/HubitatActive/master/bleBoxDevices/Drivers/wLightBoxMono.groovy"
 			   ) {
 		capability "Light"
 		capability "Switch"
