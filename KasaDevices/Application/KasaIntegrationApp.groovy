1c1d4844e8603883101f55cfb2e67017d3f01812
David Gutheinz <djgutheinz@outlook.com>
2020-05-21 11:28:57 -0500

    Add files via upload

--- KasaDevices/Application/KasaIntegrationApp.groovy
+++ KasaDevices/Application/KasaIntegrationApp.groovy
@@ -98,7 +98,7 @@ def parseDeviceData(response) {
 	def clearResp = inputXOR(resp.payload)
 	if (clearResp.length() > 1022) {
 		if (clearResp.indexOf("HS300") != -1) {
-			state.hs300Error = "Parsing failed due to return length too long.\n" +
+			state.hs300Error = "<b>HS300 Error: </b>Parsing failed due to return length too long.\n" +
 			"<b>Probable cause::</b> For the HS300, the names for the six plugs must not exceed a " +
 			"total of 102 characters (or less}. \n<b>Using the Kasa App, " + 
 			"shorten the HS300 plug names and try again.</b>"
@@ -245,7 +245,7 @@ def mainPage() {
 				description: "Bind/Unbind/Reboot Devices"
 			paragraph "<b>${count} Devices are in the Application Database</b>"
 			paragraph "<textarea rows=10 cols=40 readonly='true'>${foundDevices}</textarea>"
-			paragraph "<b>HS300 Error</b>:  ${state.hs300Error}"
+			paragraph state.hs300Error
 			input "debugLog", "bool", 
 				title: "Enable debug logging for 30 minutes", 
 				submitOnChange: true,
